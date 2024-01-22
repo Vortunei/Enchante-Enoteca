9be858cc093573031eee8a7ce3e2d50ab89d61ce
Gabriel Rojas <grojas94@yahoo.com>
2024-01-21 16:45:57 -0800

    first push

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+console.log("Massandje, Emma, and Gabriel");
+*/
+
+var audio = document.getElementById("myAudio");
+
+
+function playAudio() {
+    audio.play();
+}
+
+
+function pauseAudio() {
+    audio.pause();
+}
+
+
+function setVolume(volume) {
+    audio.volume = volume; 
+}
+
+
+
+
